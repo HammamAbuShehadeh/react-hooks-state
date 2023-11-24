ea9052472f64094cda8ef4715c95deac5891d17c
Hammam AbuShehadeh <hammam.y.asmar@gmail.com>
2023-11-24 22:52:19 +0300

    Completed assignment

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+/*
 function Counter() {
   const [count, setCount] = useState(0);
 
@@ -11,3 +11,42 @@ function Counter() {
 }
 
 export default Counter;
+*/
+/*
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    // console.log(`before setState: ${count}`);
+    setCount(count + 1);
+    console.log(count);
+    setCount(count + 1);
+    console.log(count);
+    // will not increse twice the second setCount it will not work to solve this use arrow function
+    // console.log(`after setState: ${count}`);
+  }
+
+  return <button onClick={increment}>I have been clicked {count} times</button>;
+}
+export default Counter;
+*/
+
+function Counter() {
+  const [currentCount, setCount] = useState(0);
+
+  function increment() {
+    //console.log(`before setState: ${count}`);
+    setCount((currentCount) => currentCount + 1);
+    console.log(currentCount);
+    setCount((currentCount) => currentCount + 1);
+    console.log(currentCount);
+    // the second setCount will take value from first one 
+    //console.log(`after setState: ${count}`);
+  }
+
+  return (
+    <div onClick={increment}>I have been clicked {currentCount} times</div>
+  );
+}
+export default Counter;
+
